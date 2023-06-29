f3be89edeffa1df8c9f9e6bd44243ed5fce35013
Dario Esposito <dario.esposito@moneyfarm.com>
2023-06-29 09:43:35 +0200

    modificate le config

--- teleborsaconfig.py
+++ teleborsaconfig.py
@@ -3,4 +3,9 @@ mapping={'XS2278330928':'https://www.teleborsa.it/covered-warrants-estero/sgi-in
          'DE000HV4FA47':'https://www.teleborsa.it/covered-warrants-estero/uch-ftse-mib-cc-070824-ov4fa4-de000hv4fa47-MjMuMU9WNEZBNA',
          'DE000HB0ST13':'https://www.teleborsa.it/sedex-securitised-derivates/uch-cc-ac-wo-bpsgdb-60-2-45-161025-ub0st1-de000hb0st13-REUwMDBIQjBTVDEz',
          'DE000HC1C4T4':'https://www.teleborsa.it/sedex-securitised-derivates/uch-cc-memo-ac-bdbscg-100-60-161025-uc1c4t-de000hc1c4t4-REUwMDBIQzFDNFQ0',
-         'DE000HC1C558':'https://www.teleborsa.it/sedex-securitised-derivates/uch-cc-memo-ac-dlndse-100-60-161025-uc1c55-de000hc1c558-REUwMDBIQzFDNTU4'}
+         'DE000HC1C558':'https://www.teleborsa.it/sedex-securitised-derivates/uch-cc-memo-ac-dlndse-100-60-161025-uc1c55-de000hc1c558-REUwMDBIQzFDNTU4',
+         'DE000VV1KCG7':'https://www.teleborsa.it/sedex-securitised-derivates/von-mmccbarxp-hfrdeh-60-27122023-f23521-de000vv1kcg7-REUwMDBWVjFLQ0c3',
+         'DE000VU6UTC9':'https://www.teleborsa.it/sedex-securitised-derivates/von-memccbarxp-unicredit-11-11-040526-f36221-de000vu6utc9-REUwMDBWVTZVVEM5',
+         'DE000VU6EKD0':'https://www.teleborsa.it/sedex-securitised-derivates/von-mmccbarxp-sesrnl-55-241024-f36092-de000vu6ekd0-REUwMDBWVTZFS0Qw',
+         'DE000VU6RER5':'https://www.teleborsa.it/sedex-securitised-derivates/von-mmccbarxp-pirelli-stla-stm-60-281024-f36170-de000vu6rer5-REUwMDBWVTZSRVI1'
+}
